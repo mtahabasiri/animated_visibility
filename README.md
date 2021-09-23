199d738b0e7f318c1919b344728160d201b030a5
MohammadTaha Basiri <m.taha.basiri@gmail.com>
2021-09-23 23:27:10 +0330

    v1.0.0

---
+++ README.md
@@ -0,0 +1,41 @@
+## animated_visibility
+
+[![Awesome Flutter](https://img.shields.io/badge/Awesome-Flutter-blue.svg?longCache=true&style=flat-square)](https://github.com/Solido/awesome-flutter)
+
+
+A package to easily animate visibility of an element in multichild widgets like rows, columns, listviews and more.
+
+![image info](./example/demo.gif)
+
+## Getting started
+
+
+### With Flutter:
+```
+flutter pub add animated_visibility
+```
+
+### This will add a line like this to your package's pubspec.yaml (and run an implicit flutter pub get):
+```
+dependencies:
+  simple_animations: ^4.0.0
+```
+
+
+## Usage
+
+```dart
+     AnimatedVisibility(
+            isVisible: _isVisible, //whether widget should be visible or hidden
+            duration: const Duration(milliseconds: 400), //duration of transition animation
+            curve: Curves.easeIn, //cure of animation. default is easeIn
+            fadeMode: FadeMode.horizontal, //direction of animation
+            child: Container(
+              height: 70,
+              width: double.infinity,
+              color: Colors.orangeAccent,
+              alignment: Alignment.center,
+              child: const Text('Dynamic box'),
+            ),
+          ),
+```
---
+++ lib/animated_visibility.dart
@@ -0,0 +1,90 @@
+library animated_visibility;
+
+import 'package:flutter/material.dart';
+
+enum FadeMode {
+  vertical,
+  horizontal,
+  both,
+}
+
+class AnimatedVisibility extends StatefulWidget {
+  final bool isVisible;
+  final Duration duration;
+  final Widget child;
+  final Curve curve;
+  final FadeMode fadeMode;
+
+  const AnimatedVisibility({
+    Key? key,
+    required this.isVisible,
+    required this.duration,
+    required this.child,
+    this.fadeMode = FadeMode.both,
+    this.curve = Curves.easeIn,
+  }) : super(key: key);
+
+  @override
+  _AnimatedVisibilityState createState() => _AnimatedVisibilityState();
+}
+
+class _AnimatedVisibilityState extends State<AnimatedVisibility>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _sizeController;
+  late final Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _setupAnimation();
+    _runSizeCheck();
+  }
+
+  void _setupAnimation() {
+    _sizeController =
+        AnimationController(vsync: this, duration: widget.duration);
+    _animation = CurvedAnimation(
+      parent: _sizeController,
+      curve: widget.curve,
+    );
+  }
+
+  @override
+  void didUpdateWidget(covariant AnimatedVisibility oldWidget) {
+    _runSizeCheck();
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void _runSizeCheck() {
+    if (widget.isVisible) {
+      _sizeController.forward();
+    } else {
+      _sizeController.reverse();
+    }
+  }
+
+  @override
+  void dispose() {
+    _sizeController.dispose();
+    super.dispose();
+  }
+
+  bool get _isVertical =>
+      widget.fadeMode == FadeMode.vertical || widget.fadeMode == FadeMode.both;
+  bool get _isHorizontal =>
+      widget.fadeMode == FadeMode.horizontal ||
+      widget.fadeMode == FadeMode.both;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      child: widget.child,
+      builder: (context, child) => Align(
+        heightFactor: _isVertical ? _animation.value : null,
+        widthFactor: _isHorizontal ? _animation.value : null,
+        child: Opacity(opacity: _animation.value, child: child),
+      ),
+    );
+  }
+}
